5034c6a4f7b6284c953c0073b8eb064c62b51aeb
WMLPUG <99414205+WMLPUG@users.noreply.github.com>
2022-02-11 09:22:14 -0500

    OrdenarPrecio.java
    Oli

---
+++ OrdenarPrecio.java
@@ -0,0 +1,18 @@
+
+import java.util.Comparator;
+
+public class OrdenarPrecio implements Comparator<Libro>{
+
+	@Override
+	public int compare(Libro l1, Libro l2) {
+		if(l1.getPrecio()>l2.getPrecio()) {
+			return -1;
+		}
+		if(l1.getPrecio()<l2.getPrecio()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+}
