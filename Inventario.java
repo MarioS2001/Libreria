736a3026cd581df27d4e59149d69647cc94f5077
MarioS2001 <99413031+MarioS2001@users.noreply.github.com>
2022-02-13 15:15:42 -0500

    Update Inventario.java

--- Inventario.java
+++ Inventario.java
@@ -23,29 +23,6 @@ public class Inventario {
 		return libros;
 	}	
 	
-	public void ventaLibros(String isbn, int cantidad) {
-		if(getLibrobyIsbn(isbn) == null) {
-			System.out.println("\nISBN incorrecto\n");}
-		else
-			if(getLibrobyIsbn(isbn).getCantidad() >= cantidad) {
-				System.out.println("\n\t\t-- VENTA--");
-				System.out.print("\nISBN: "+ isbn);
-				System.out.print("\nTítulo: "+ getLibrobyIsbn(isbn).getTitulo());
-				System.out.print("\nPrecio: "+ getLibrobyIsbn(isbn).getPrecio());
-				System.out.print("\nCantidad: "+ cantidad);
-				System.out.print("\nTotal a pagar: $"+
-				getLibrobyIsbn(isbn).getPrecio()*cantidad+"\n");
-				getLibrobyIsbn(isbn).setCantidad((getLibrobyIsbn(isbn).getCantidad() - cantidad));
-				if(getLibrobyIsbn(isbn).getCantidad() == 0) {
-					System.out.println("\nLibro agotado\n");
-					libros.remove(libros.indexOf(getLibrobyIsbn(isbn)));
-					}
-			} else {
-				System.out.println("\nLa cantidad requerida sobrepasa la cantidad de este libro");
-				System.out.println("\nSolo hay "+getLibrobyIsbn(isbn).getCantidad()+" unidades\n");
-			}
-	}
-	
 	public void ordenarPrecios() {
 		ordenPrecio = new ArrayList<Libro>();
 		for(int i=0; i<libros.size(); i++) {
@@ -62,12 +39,6 @@ public class Inventario {
 		return null;
 	}
 	
-	public String getlibrosCompletos() {
-		for(int i=0; i<libros.size(); i++){
-			System.out.println(libros.get(i));
-		}
-		return null;	
-	}
 }
 
 	
